62a1e93ce8dd5d70f93e9226c3ec874a85bb84c4
Elham Hamzapour <ehamzapour@gmail.com>
2023-01-22 17:33:25 -0600

    imported component js files in app.js

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,7 +1,28 @@
+import React, { useState } from 'react';
+import './app.css';
+import Resume from './components/resume/Resume';
+import About from './components/about/About';
+import Header from './components/header/Header';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+
+
+
 function App() {
   return (
     <div className="app">
-        hello
+      <Header></Header>
+      <div className='sections'>
+        <About></About>
+        <Portfolio></Portfolio>
+        <Contact></Contact>
+        <Resume></Resume>
+        <Footer></Footer>
+      </div>
+      <div className='footer'>
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
--- react-portfolio/src/components/about/About.js
+++ react-portfolio/src/components/about/About.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './about.css';
+import { useEffect, useRef } from 'react';
+import { init } from 'ityped'
 
 function About() {
+  const textRef = useRef();
+
+  useEffect (()=>{
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['marketer', 'leader', 'full stack developer', 'badass'],
+    });
+
+  }, []);
+
   return (
-    <div className='about'>About</div>
-  )
+    <div className='about' id='about'>
+      <div className='left'>
+        <div className='imgContainer'>
+          {/* add profile pic here */}
+          <img src='#' alt='user profile pic'/>
+        </div>
+      </div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi, I'm</h2>
+          <h1>Elham Hamzapour</h1>
+          <h3><span ref={textRef}></span> Developer</h3>
+          <p>BIO ABOUT ME</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default About;
